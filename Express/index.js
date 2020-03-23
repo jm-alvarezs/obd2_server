3d674352e440da82af09321d21232d9c576ecff0
jm-alvarezs <jm.alvarezs98@gmail.com>
2020-03-23 12:24:54 -0600

    added sentry

--- Express/index.js
+++ Express/index.js
@@ -5,9 +5,15 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const db = require("tnc_mysql_connector");
 const chalk = require("chalk");
+const Sentry = require('@sentry/node');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+Sentry.init({ dsn: 'https://b74a035d1d2f4e89b8939f599c9e4ab7@sentry.io/5172247' });
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
 
 app.use(
   cors({
@@ -31,6 +37,19 @@ require("dotenv").config();
 db.connect().then(async () => {
 
   require("./startup/routes")("/api", app);
+
+  // The error handler must be before any other error middleware and after all controllers
+  app.use(Sentry.Handlers.errorHandler());
+
+  // Optional fallthrough error handler
+  app.use(function onError(err, req, res, next) {
+    // The error id is attached to `res.sentry` to be returned
+    // and optionally displayed to the user for support.
+    if(process.env.NODE_ENV !== "development") {
+      res.statusCode = 500;
+      res.end(res.sentry + "\n");
+    };
+  });
 
   app.listen(port, () => {
     console.log("Embebidos Server listening on port " + port);
